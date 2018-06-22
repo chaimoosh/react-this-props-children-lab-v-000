278fc2725cae82f0c63801161cda837a2b3b582b
Chaim Reisman <chaim@Chaims-MacBook-Pro.local>
2018-06-22 11:39:22 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,15 @@
 // Code Invitation Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class Invitation extends React.Component {
+  render () {
+    return (
+      <div className="invitation">
+        <h1>You've been invited!</h1>
+        <p>{this.props.children}</p>
+      </div>
+    )
+  }
+}
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,16 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class ThemedDecoration extends React.Component {
+  render () {
+    return (
+      <div>
+        <div className={this.props.theme}>{this.props.chidren}</div>
+        <div className={this.props.theme}>{this.props.chidren}</div>
+        <div className={this.props.theme}>{this.props.chidren}</div>
+      </div>
+    )
+  }
+}
